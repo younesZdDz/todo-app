a0ca2acecc4d39d119e5b00aefffef724a2dee3a
MAC <mac@MacBook-Pro-de-MAC.local>
2020-10-18 20:38:29 +0200

    refact: refactored todos state to cutom hook

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,38 +1,20 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect } from 'react';
 import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
-import { v4 as uuidv4 } from 'uuid';
 import TodoList from './TodoList';
 import TodoForm from './TodoForm'; 
+import useTodosState from './hooks/useTodosState'; 
 
 function TodoApp () {
     const initialTodos = JSON.parse(window.localStorage.getItem('todos') ||  '[]');
-    const [todos, setTodos] = useState(initialTodos);
+    const {todos, addTodo, deleteTodo, toggleTodo, editTodo} = useTodosState(initialTodos);
     useEffect(()=>{
       window.localStorage.setItem('todos', JSON.stringify(todos));
     }, [todos]);
-    const addTodo = (todoText) =>{
-      setTodos([...todos, {id: uuidv4(), title: todoText, isDone: false}])
-    }
-
-    const deleteTodo = (todoId) => {
-      const updatedTodos = todos.filter((todo) => todo.id !== todoId);
-      setTodos(updatedTodos);
-    }
-
-    const toggleTodo = (todoId) =>{
-      const updatedTodos = todos.map((todo) => todo.id === todoId ? {...todo, isDone: !todo.isDone} : todo);
-      setTodos(updatedTodos);
-    }
-
-    const editTodo = (todoId, newTitle) => {
-      const updatedTodos = todos.map((todo) => todo.id === todoId ? {...todo, title: newTitle} : todo);
-      setTodos(updatedTodos);
-    }
+    
     return(
         <Paper style={{
             margin:0,
